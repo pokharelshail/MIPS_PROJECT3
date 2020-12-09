0c8023c0e19c0667ddb4bbf4612b510e4bf0d5fb
pokharelshail <shreeshail.pokharel@bison.howard.edu>
2020-12-08 22:05:18 -0500

    Load input to register

--- project3.s
+++ project3.s
@@ -8,3 +8,6 @@
 		la $a0, input
 		li $a1, 1001
 		syscall
+        la $s0, input   #load input to register
+        li $s1, 0    #The pointer where it begins
+        li $s2, 0  #Pointer where it ends
